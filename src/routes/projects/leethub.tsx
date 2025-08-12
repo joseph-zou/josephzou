81c93dbdc6134f8f595e4f953ea252e07045d6c0
Joseph <joeyspec0715@gmail.com>
2025-08-11 18:24:38 -0700

    .

--- src/routes/projects/leethub.tsx
+++ src/routes/projects/leethub.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/projects/leethub')({
+export const Route = createFileRoute("/projects/leethub")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/projects/leethub"!</div>
+  return (
+    <div>
+      Hello "/projects/leethub"! This one was a doozy, I had to interact with
+      express-session, github oauth + github app installation, work with chrome
+      extensions content and background scripts and a bunch of dom elements.
+      Also had to make multiple config files because each environment needed its
+      own build settings.
+    </div>
+  );
 }
